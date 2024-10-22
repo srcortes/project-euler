c9339af8028573db91add0c612529bfc214d8e15
john.rodriguez <john.rodriguez@gmail.com>
2024-10-21 23:17:32 -0500

    In order to practice algorithms

---
+++ src/amadeustest/TechnicalInterview.java
@@ -0,0 +1,40 @@
+package amadeustest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class TechnicalInterview {
+
+    public static void main(String[] args) {
+        System.out.println(percentageLetter("foobar", 'o'));
+        System.out.println(percentageLetter("jjjj", 'k'));
+        System.out.println(countWords(new String[]{"code", "is", "amazing", "as", "is"},
+                new String[]{"amazing","code","is"}));
+        System.out.println(countWords(new String[]{"b","bb","bbb"},
+                new String[]{"a","aa","aaa"}));
+        System.out.println(countWords(new String[]{"a","ab"},
+                new String[]{"a","a","a","ab"}));
+    }
+
+    private static int percentageLetter(String s, char letter) {
+        double amount = 0;
+        for(char value : s.toCharArray())
+            if(value == letter)
+                amount += 1;
+        return Double.valueOf(amount  / s.length() * 100).intValue();
+    }
+
+    private static int countWords(String[] words1, String[] words2) {
+        List<String> list1 = new ArrayList<>(Arrays.asList(words1));
+        List<String> list2 = new ArrayList<>(Arrays.asList(words2));
+        list1.addAll(list2);
+        Map<String, Long> map = list1.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        map.values().removeIf(i -> i != 2);
+        return map.entrySet().size();
+    }
+}
--- src/multiplenumber/MultipleNumberThreeOrFive.java
+++ src/multiplenumber/MultipleNumberThreeOrFive.java
@@ -1,7 +1,22 @@
 package multiplenumber;
 
+import java.util.stream.IntStream;
 public class MultipleNumberThreeOrFive {
     public static void main(String[] args) {
-        System.out.println("...");
+        System.out.println(calculate());
+        System.out.println(newCalculate(3) + newCalculate(5) - newCalculate(15));
+    }
+    private static int calculate(){
+      return IntStream.rangeClosed(0,999).map(j -> {
+           int outcome = 0;
+           if(j % 3 == 0 || j % 5 == 0)
+               outcome = j;
+           return outcome;
+       }).sum();
+    }
+
+    private static int newCalculate(int value){
+        int p = 999 / value;
+        return value * (p * (p + 1)) / 2;
     }
 }
