14329b593d7a6c4cc2fa295bc030aaef7cfe49ae
john.rodriguez <john.rodriguez@gmail.com>
2024-12-09 23:09:27 -0500

    In order to improve algorithm large palinndrome

--- src/largepalindromeproduct/LargePalindromeProduct.java
+++ src/largepalindromeproduct/LargePalindromeProduct.java
@@ -8,25 +8,42 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LargePalindromeProduct {
+
+    private static List<Integer> outcome = new ArrayList<>();
     public static void main(String[] args) {
+        LargePalindromeProduct.calculatePalindromeNumberOptimizationV1();
+        //LargePalindromeProduct.calculatePalindromeNumberOptimizationV2();
+
+    }
+
+    private static void calculatePalindromeNumberOptimizationV1(){
         List<Integer> list = IntStream.rangeClosed(100, 999).boxed()
                 .collect(Collectors.toList());
-        List<Integer> outcome = new ArrayList<>();
         for(int j = list.size()-1; j >= 0 ; j--){
             for(int i = j; i >= 0; i--){
-                int value =  list.get(j) * list.get(i);
-                StringBuilder builder = new StringBuilder(String.valueOf(value)).reverse();
-                if(String.valueOf(value).contentEquals(builder))
-                    outcome.add(value);
+                int value = list.get(j) * list.get(i);
+                acumulateNumberPalindrome(value);
             }
         }
         System.out.println(outcome.stream().max(Comparator.comparing(Function.identity())).get());
+
+    }
+    private static void acumulateNumberPalindrome(int value){
+        String number = Integer.toString(value);
+        int increment = 0;
+        int decrement = number.length() - 1;
+        boolean isNotPalindrome = false;
+        while(increment < decrement && !isNotPalindrome){
+            if(number.charAt(increment) == number.charAt(decrement)){
+                increment++;
+                decrement--;
+            } else {
+                isNotPalindrome = true;
+            }
         }
-
-
-
-
-
+        if(!isNotPalindrome)
+            outcome.add(value);
+    }
 
 
 }
