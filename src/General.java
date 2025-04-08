b47cad7aad0b6cdec7aabc66d44549b4dc201abe
john.rodriguez <john.rodriguez@gmail.com>
2025-04-07 23:03:15 -0500

    practice

--- src/General.java
+++ src/General.java
@@ -7,42 +7,77 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 
-public class General {
+interface Producto {
+    String getNombre();
+    double getPrecio();
+}
 
-    public static void main(String[] args) {
-        General general = new General();
-        /*Integer nums[] = {1, 2, 2, 4, 5, 6, 7, 7, 9, 10};
-        Stream<Integer> stream = Stream.of(nums);
-        List<Integer> map = stream.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet().stream().distinct()
-                .map(Map.Entry::getKey).toList();
-        nums = map.toArray(new Integer[0]);
-        System.out.println(Arrays.toString(nums));*/
+class ProductoImpl implements Producto {
+    private String nombre;
+    private double precio;
 
-
-        int nums2[] = {1, 3, 6, 4, 1, 2};
-        System.out.println(general.solution(nums2));
-        /*int nums3[] = {1, 2, 3};
-        System.out.println(general.solution(nums3));
-        int nums4[] = {-1, -3};
-        System.out.println(general.solution(nums4));*/
-
-
+    public ProductoImpl(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
     }
 
-    public int solution(int[] A) {
-        int min = Arrays.stream(A).min().orElse(0);
-        for(int j = 0; j < A.length; j++) {
-            if(A[j] < 0){
-                return 1;
-            }
-            if(A[j] > 0){
-                if(A[j] == (min)) {
-                    min++;
-                }
-            }
-        }
-        return min;
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public double getPrecio() {
+        return precio;
+    }
+}
+
+class ProductDomain{
+
+    private String nombre;
+    private List<Producto> productos = new ArrayList<>();
+
+    public void addProducto(Producto producto){
+        productos.add(producto);
+    }
+
+    public Map<String, Double> getProductoMap(){
+        return productos.stream().collect(Collectors.toMap(Producto::getNombre, Producto::getPrecio));
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+}
+
+public class General {
+
+    public static void algo(Producto producto){
+        ProductDomain productDomain = new ProductDomain();
+        productDomain.addProducto(producto);
+        productDomain.setNombre(productDomain.getNombre());
+        System.out.println(productDomain.getProductoMap());
+    }
+
+    public static void main(String[] args) {
+        ProductoImpl producto = new ProductoImpl("Producto 1", 100.0);
+        algo(producto);
+
+
+
+
     }
 
 }
--- src/latamairlinetest/SumDiagonals.java
+++ src/latamairlinetest/SumDiagonals.java
@@ -9,10 +9,10 @@ public class SumDiagonals {
 
         List<List<Integer>>  list = Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6), Arrays.asList(7, 8, 9));
 
-
         int principalDiagonal = IntStream.range(0, list.size()).map(i -> list.get(i).get(i))
                 .peek(System.out::println)
                 .sum();
+
 
 
         int  secondaryDiagonal = IntStream.range(0, list.size())
