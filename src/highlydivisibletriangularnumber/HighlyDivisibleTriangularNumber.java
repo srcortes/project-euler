4b469a2306b3fdae6b8763d8331a816d3cbc330a
john.rodriguez <john.rodriguez@gmail.com>
2025-04-22 23:38:11 -0500

    practicing

---
+++ src/highlydivisibletriangularnumber/HighlyDivisibleTriangularNumber.java
@@ -0,0 +1,30 @@
+package highlydivisibletriangularnumber;
+
+public class HighlyDivisibleTriangularNumber {
+    static boolean flag = true;
+
+    public static void main(String[] args) {
+            int val = 0;
+            int outcome = 0;
+            while(flag){
+                ++val;
+                int value = val * (val + 1 ) / 2;
+                outcome = verifyDivisors(value);
+            }
+        System.out.println(outcome);
+    }
+    public static int verifyDivisors(int value) {
+       int count = 0;
+       int sqrt = (int) Math.sqrt(value);
+
+        for(int j = 1; j < sqrt; j++){
+             if(value % j == 0)
+               count += 2;
+        }
+
+        if( count >= 500)
+            flag = false;
+
+       return value;
+    }
+}
