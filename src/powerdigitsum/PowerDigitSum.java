1f1c8a0938d27c9d8cf80d9c40b2eb84d8c6d7c9
john.rodriguez <johnrodriguez.globant@latam.com>
2025-09-05 16:21:31 -0500

    practice

---
+++ src/powerdigitsum/PowerDigitSum.java
@@ -0,0 +1,21 @@
+package powerdigitsum;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PowerDigitSum {
+    public static void main(String[] args) {
+
+       List<BigInteger> digits = new ArrayList<>();
+       String twoToThePowerOfFifteen = BigInteger.valueOf(2).pow(1000).toString();
+
+        for(Character c : twoToThePowerOfFifteen.toCharArray()) {
+            digits.add(new BigInteger(c.toString()));
+        }
+
+        System.out.println(digits.stream().mapToInt(BigInteger::intValue).sum());
+    }
+}
